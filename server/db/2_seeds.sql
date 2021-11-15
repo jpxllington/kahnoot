487cb4885ba3c2b3846597c3475e04b70c9dda50
olu-akano <akanopaul@yahoo.co.uk>
2021-11-15 12:14:29 +0000

    add seeds to db

---
+++ server/db/2_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO leaderboard (name, difficulty, topic, score)
+VALUES
+    ('Bob', 'Easy', 'History', 6)
+    ('Golnar', 'Medium', 'Geography', 8)
+    ('Calum', 'Easy', 'Politics', 4)
+    ('Cameron', 'Hard', 'Sports', 3)
+    ('James', 'Medium', 'Art', 7)
+    ('Olu', 'Hard', 'General Knowledge', 8)
