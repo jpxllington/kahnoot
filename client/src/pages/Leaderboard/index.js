008d4d252538890c74660e8390acd9e8878ebdeb
glnkvn <golnarkavianii@gmail.com>
2021-11-15 12:39:58 +0000

    add a return function for Leaderboard page

--- client/src/pages/Leaderboard/index.js
+++ client/src/pages/Leaderboard/index.js
@@ -8,7 +8,8 @@ import './style.css';
 const Leaderboard = () => {
 
     const [ category, setCategory ] = useState('');
-
+    const [ scores , setScores ] = useState();
+    const [ difficulty, setDifficulty ] = useState('')
     const categories=['Sports','History','General knowledge','Entertainment','Celeberties'];
 
     const data={"scores": [{"id":"1","username":"Bob","category":"General Knowledge","difficulty":"easy","score":9},
@@ -18,6 +19,69 @@ const Leaderboard = () => {
     {"id":"5","username":"Tom","category":"Sports","difficulty":"easy","score":10}]}
 
     const history = useHistory();
+
+    const sortScores = (x, y) => {
+        return x.score - y.score;  //return a sorted data in descending order of score
+    }
+
+
+    useEffect(() => {
+        async function getScores() {
+            try {
+                const filteredScores = await data.filter(d => d.category == category && d.difficulty == difficulty);
+                filteredScores.sort(sortScores);
+                setScores(filteredScores);
+            } catch(error) {
+                console.log(error.message);
+            }
+        } 
+        getScores();
+    }, [category, difficulty])
+
+
+
+    return (
+        <>
+            <div>
+                <h1>High Scores!</h1>
+            </div>
+            
+            <form id="scores">
+                <div className="custom-select">
+                    <label htmlFor="testTopic">Test topic</label>
+                    <select name="testTopic" id="testTopic" onChange={(e) => setCategory(e.target.value)}>
+                        {categories.map((d,i) => <option key={i}>{d.category}</option>)}
+                    </select>
+                    <label htmlFor="difficulty">Difficaulty</label>
+                    <select name="difficulty" id="difficulty" onChange={(e)=> setDifficulty(e.target.value)}>
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+                </div>
+            </form>
+
+            <table id="rankings" className="table">
+			<thead>
+				<tr>
+					<th>Rank</th>
+					<th>Username</th>
+					<th>Score</th>
+				</tr>
+			</thead>
+			<tbody>
+                {scores && 
+                    scores.map((x, i ) => <tr key={i}>
+                                            <td>{i+1}</td>
+                                            <td>{x.username}</td>
+                                            <td>{x.score}</td>
+                                        </tr>)
+                                        }
+			</tbody>
+		</table>
+
+        </>
+    )
 
    
 }
