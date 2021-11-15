d50b7afaeb88ef1d25ecb1df34e6547500f501c0
glnkvn <golnarkavianii@gmail.com>
2021-11-15 14:05:48 +0000

    Small updates to the code

--- client/src/pages/Leaderboard/index.js
+++ client/src/pages/Leaderboard/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 import './style.css';
 
 
