24658c7ea858d8df9b6419049ea0a3197ee91732
glnkvn <golnarkavianii@gmail.com>
2021-11-15 14:05:26 +0000

    Small updates to the code

--- client/src/pages/Leaderboard/index.js
+++ client/src/pages/Leaderboard/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-// import axios from 'axios';
+import axios from 'axios';
 import './style.css';
 
 
@@ -10,31 +10,38 @@ export const Leaderboard = () => {
     const [ category, setCategory ] = useState('');
     const [ scores , setScores ] = useState();
     const [ difficulty, setDifficulty ] = useState('');
+    // const [ data, setData ] = useState('');
 
     const deficultis=['Easy','Medium','Hard'];
     const categories=['Sports','History','General Knowledge','Entertainment','Celeberties'];
 
-    const data={"scores": [{"id":"1","username":"Bob","category":"General Knowledge","difficulty":"Easy","score":9},
+    const data=[{"id":"1","username":"Bob","category":"General Knowledge","difficulty":"Easy","score":9},
     {"id":"2","username":"Kelly","category":"General Knowledge","difficulty":"Easy","score":6},
     {"id":"3","username":"Emma","category":"General Knowledge","difficulty":"Easy","score":7},
     {"id":"4","username":"Alex","category":"History","difficulty":"Easy","score":4},
-    {"id":"5","username":"Tom","category":"Sports","difficulty":"Easy","score":10}]}
+    {"id":"5","username":"Tom","category":"Sports","difficulty":"Easy","score":10}]
 
     const history = useHistory();
 
-    const sortScores = (x, y) => {
-        return x.score - y.score;  //return a sorted data in descending order of score
+    //use this to fetch the data from storage
+    // useEffect(async () => {
+    //     try {
+    //         let { data } = await axios.get('url'); //fetch the players data
+    //         setData(data.scores);
+    //     } catch (error) {
+    //         console.log(error.message);
+    //     }
+    // }, []);
+
+    function sortScores(x, y) {
+        return x.score - y.score; //return a sorted data in descending order of score
     }
 
 
     useEffect(() => {
         async function getScores(){
             try{
-                const playersData=data.scores;
-                console.log(playersData);
-                console.log(category);
-                console.log(difficulty);
-                const filteredScores = await playersData.filter(d => d.category == category && d.difficulty == difficulty);
+                const filteredScores = await data.filter(d => d.category == category && d.difficulty == difficulty);
                 filteredScores.sort(sortScores);
                 setScores(filteredScores);
             } catch(error) {
