1a05ceff65b8fe573360541e927d3705e0ba7574
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-16 14:55:01 +0000

    cleanup commented out code

--- client/src/pages/quizPage/index.jsx
+++ client/src/pages/quizPage/index.jsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import { Quiz } from "../../components";
 import { Question } from "../../components";
 import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchQuiz, changeQ } from '../../actions';
+import { changeQ } from '../../actions';
 import he from 'he';
 
 export const QuizPage = () => {
 
     const [answers, setAnswers] = useState([])
     const [question, setQuestion] = useState("")
-    // const [apiData, setApiData] = useState([])
-    // const [currentQ, setCurrentQ] = useState()
-    // const [chosenAnswer, setChosenAnswer] = useState("")
     const [correctAnswer, setCorrectAnswer] = useState("")
-    // const [timer, setTimer] = useState();
+
     let currentQ = useSelector(state => state.currentQ);
     const apiData = useSelector(state => state.apiData);
     console.log(apiData);
@@ -24,63 +20,25 @@ export const QuizPage = () => {
     const history = useHistory();
     const dispatch = useDispatch();
 
-    // const handleAnswer = async (e) => {
-    //     e.preventDefault()
-    //     console.log(e.target.textContent);
-    //     setChosenAnswer(e.target.textContent);
-    // }
-
-    // useEffect(() => {
-    //     let amount = 10;
-    //     let category = 23;
-    //     let difficulty = "easy";
-    //     dispatch(fetchQuiz(amount, category, difficulty))
-    //     // async function callAPI() {
-    //     //     let amount = 10;
-    //     //     let category = 23;
-    //     //     let difficulty = "easy";
-    //     //     let { data } = await axios.get(`https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`)
-    //     //     setApiData(data.results);
-    //     //     setCurrentQ(0);
-    //     //     // recievesQuestionData(data.results[0])
-
-    //     // }
-    //     // callAPI()
-    // }, [])
     const authenticate = (chosenAnswer) => {
         correct = !!chosenAnswer && chosenAnswer === correctAnswer;
         console.log(correct);
         console.log(correctAnswer);
         console.log(chosenAnswer);
-        // const qIndex = apiData.findIndex(x => x.question === question);
-        // const qIndex = apiData.map(d => d.question).indexOf(question);
-        // correctAnswers = correctAnswer === chosenAnswer
-        // console.log(correctAnswers);
-        // if (correctAnswer === chosenAnswer) {
-        //     console.log("answer authenticated");
-        // } else {
-        //     console.log("wrong");
-        // }
     }
-
-
-    // setTimer()
 
     function shuffle(array) {
         let currentIndex = array.length, randomIndex;
 
         // While there remain elements to shuffle...
         while (currentIndex != 0) {
-
             // Pick a remaining element...
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-
             // And swap it with the current element.
             [array[currentIndex], array[randomIndex]] = [
                 array[randomIndex], array[currentIndex]];
         }
-
         return array;
     }
 
@@ -88,19 +46,8 @@ export const QuizPage = () => {
     const timerDone = (qIndex) => {
         qIndex === apiData.length ? history.push('/results')
             : dispatch(changeQ())
-        // : recievesQuestionData(apiData[qIndex])
         console.log(apiData[qIndex]);
         console.log("next question");
-        // const authenticate = (chosenAnswer) => {
-        //     console.log(correctAnswer);
-        //     console.log(chosenAnswer);
-        //     if(correctAnswer===chosenAnswer){
-        //         console.log("answer authenticated");
-        //     } else{
-        //         console.log("wrong");
-        //     }
-        // }
-        // authenticate(chosenAnswer)
     }
 
     useEffect(() => {
