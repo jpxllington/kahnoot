d637b95ef9736a3764c011e26202b414887014a4
jpxllington <jjdp99@gmail.com>
2021-11-16 18:01:17 +0000

    add most needed functions to socket

--- socket/model.js
+++ socket/model.js
@@ -1,26 +1,56 @@
 const { timeStamp } = require("console");
 const { threadId } = require("worker_threads");
 
-class Game{
+export class Game{
 
     constructor(){
         this.games = [];
         this.players = [];
     }
 
-    addGame(hostID,roomID, difficulty, amount, subject){
+    addGame(hostID,roomName, ApiData){
         let game = {
             host:hostID,
-            room:roomID,
-            difficulty:difficulty,
-            amount:amount,
-            subject:subject,
+            room:roomName,
             players:[],
-            active:false
+            ApiData: ApiData
         }
         this.games.push(game);
         return game;
     } 
 
-    addPlayer(username)
+    addPlayer(username, roomName){
+        let player = {
+            username:username, 
+            score: 0
+        }
+
+        let game = getRoom(roomName);
+        try{
+            game.players.push(player)
+        } catch(err){
+            console.warn(err);
+        }
+    }
+
+    getRoom(roomName){
+        const currentGame = this.game.find(game=>game.room===roomName)
+        return currentGame;
+    }
+
+    checkRoom(roomName){
+        let room = getRoom(roomName);
+        return(!!room)
+    }
+
+    addPlayerScore(score, roomName, username){
+        let room = getRoom(roomName);
+        try{
+            let player = room.players.find(player => player.username ===username)
+            player.score = score;
+            return room.players
+        } catch(err){
+            console.warn(err);
+        }
+    }
 }
