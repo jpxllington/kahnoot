083a0b3ba01a247cfba2591ec455ffa906780b6f
olu-akano <akanopaul@yahoo.co.uk>
2021-11-16 11:03:55 +0000

    add test seeds

---
+++ server/api/tests/integration/test_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO leaderboard (name, difficulty, topic, score)
+VALUES
+('Test name 1', 'Easy', 'History', 6),
+('Test name 2', 'Medium', 'Geography', 8),
+('Test name 3', 'Hard', 'Politics', 4),
