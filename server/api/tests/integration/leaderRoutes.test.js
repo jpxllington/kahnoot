a55c3c18bd48a08f7508cb8cfa664bde4d31b7a9
olu-akano <akanopaul@yahoo.co.uk>
2021-11-16 12:36:44 +0000

    test show route

---
+++ server/api/tests/integration/leaderRoutes.test.js
@@ -0,0 +1,21 @@
+describe ('leader endpoints', () => {
+    let api;
+    beforeEach(async () => {
+        await resetTestDB()
+    });
+
+    beforeAll(async () => {
+        api = app.listen(5000, () => console.log('Test server running on port 5000'))
+    });
+
+    afterAll(async () => {
+        console.log('Stopping test server')
+        await api.close()
+    })
+
+    it('should return a list of players in the leaderboard', async () => {
+        const res = await request(api).get('/leaderboard');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.length).toEqual(3);
+    });
+})
