eaa418e9a03b5cbe1a063221c06f2f346317ee9c
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-17 16:16:19 +0000

    update footer test

--- client/src/layout/Footer/Footer.test.js
+++ client/src/layout/Footer/Footer.test.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
-import {  Footer } from '.';
-import { render, screen , fireEvent} from '@testing-library/react';
+import { Footer } from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 
 
-describe('Footer', () => {  
+describe('Footer', () => {
 
     test('it renders', () => {
         render(<Footer />)
         const footer = screen.getByRole('footer')
-        expect(footer.textContent).toContain('Calum, Cameron, Golnar, James, Olu - 2021')
+        expect(footer.textContent).toContain('Calum, Cameron, Golnar, James, Olu © 2021')
         // const heading = screen.getByText('Leaderboard')
         // expect(heading.textContent).toContain('Leaderboard');
     });
