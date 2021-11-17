bf63042d92bc76cddf092b4852536bb7e38879ee
Cameron <cameronbromley8@gmail.com>
2021-11-17 12:16:14 +0000

    starting playerCard component

---
+++ client/src/components/PlayerCard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { socket } from 'socket.js';
+
+export const PlayerCard = () => {
+
+    socket.on('player', )
+
+
+    return (
+        <div className="card">
+            
+        </div>
+    )
+}
--- client/src/pages/lobby/index.jsx
+++ client/src/pages/lobby/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useHistory } from "react-router"
+
 export const Lobby = () => {
+    
     let history =useHistory();
     const handleClick = () => {
         history.push("/quiz")
     }
+    
+    
     return (
         <>
             <button onClick={handleClick}>Go to quiz</button>
