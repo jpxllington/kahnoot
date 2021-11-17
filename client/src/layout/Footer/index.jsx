0663f1dad78b5fdaa0bbb13a12d0f42ac4e7cb87
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-17 14:47:09 +0000

    update footer

--- client/src/layout/Footer/index.jsx
+++ client/src/layout/Footer/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <footer>
-            <p>Calum, Cameron, Golnar, James, Olu - 2021</p>
+            <p>Calum, Cameron, Golnar, James, Olu ­© 2021</p>
         </footer>
     )
 }
