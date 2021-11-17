bb496b2d1843ede6b88ebd45cdbe3fbd3d9e637f
glnkvn <golnarkavianii@gmail.com>
2021-11-17 14:51:25 +0000

    Add test for header

--- client/src/layout/Header/index.jsx
+++ client/src/layout/Header/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import {BrowserRouter as Router} from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
 
     return (
-        <header>
-            <Link to={"./"}>Home</Link>
-            <img src="" alt="logo" />
-            <Link to={"./leaderboard"}>View Leaderboards</Link>
+        <header role="header">
+            <Router>
+                <Link to={"./"}>Home</Link>
+                <img src="" alt="logo" />
+                <Link to={"./leaderboard"}>View Leaderboards</Link>
+            </Router>
         </header>
     )
 }
