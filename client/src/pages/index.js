ca7ecf6e2fc43e37624ff84d48146efb7b3f02c9
jpxllington <jjdp99@gmail.com>
2021-11-18 10:32:33 +0000

    fix imports again

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,7 @@
-export { HomePage } from './homepage'
+export { HomePage } from './Homepage'
 export { QuizPage } from './quizPage'
 export { Leaderboard } from './Leaderboard';
-export { CreateQuiz } from './createQuiz';
+export { CreateQuiz } from './CreateQuiz';
 export { Lobby } from './lobby';
 export { Results } from './Results'
 
